0109bc72769feda8785d2d7b84f768074c7f3e98
spiekermax <spiekermann.service@gmail.com>
2021-04-06 14:49:51 +0200

    Added toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+// Material Design
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
+
 // Internal dependencies
 import { AppRoot } from "./app.root";
 import { AppRoutingModule } from "./app-routing.module";
@@ -18,7 +23,11 @@ import { HomePage } from "./feature/page/home-page/home.page";
     imports: [
         BrowserModule,
         AppRoutingModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+
+        MatButtonModule,
+        MatIconModule,
+        MatToolbarModule
     ],
     providers: [],
     bootstrap: [AppRoot]
